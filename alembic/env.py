f0dae191b6e0a8e155b2463f6a386ea8616e0143
naveenmuppana <naveenmuppana123@gmail.com>
2026-03-29 00:01:42 +0530

    d

--- alembic/env.py
+++ alembic/env.py
@@ -3,7 +3,7 @@ from sqlalchemy import engine_from_config, pool
 from alembic import context
 from app.database import Base
 from app.models import patient, doctor
-
+from app.models import *
 config = context.config
 
 if config.config_file_name is not None:
