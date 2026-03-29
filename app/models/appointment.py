d2a7717713aaa6301b0a26071d14706b6e7e2376
naveenmuppana <naveenmuppana123@gmail.com>
2026-03-30 00:05:36 +0530

    appointment file

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Enum as SAEnum, Time, Boolean
 from sqlalchemy.orm import relationship
-from datetime import datetime, date
+from datetime import datetime, timezone
+default=lambda: datetime.now(timezone.utc)
 from app.database import Base
 from app.schemas.enums import AppointmentStatus, QueuePriority
 
@@ -24,6 +25,7 @@ class Appointment(Base):
     # Relationships
     patient = relationship("Patient", back_populates="appointments")
     doctor = relationship("Doctor", back_populates="appointments")
+    slot  = relationship("Slot", back_populates="appointment")
     waiting_list_entry = relationship("WaitingList", back_populates="appointment", uselist=False)
 
 class WaitingList(Base):
@@ -32,11 +34,13 @@ class WaitingList(Base):
     id = Column(Integer, primary_key=True, index=True)
     doctor_id = Column(Integer, ForeignKey("doctors.id"), nullable=False)
     patient_id = Column(Integer, ForeignKey("patients.id"), nullable=False)
-    appointment_date = Column(date, nullable=False)
-    preferred_time = Column(Time, nullable=True)
+    appointment_date = Column(DateTime, nullable=False)
     status = Column(String(20), default="waiting")  # waiting, promoted, expired, cancelled
+ # waiting, promoted, expired, cancelled
     position = Column(Integer, nullable=False)
     created_at = Column(DateTime, default=datetime.utcnow)
+    appointment_id = Column(Integer, ForeignKey("appointments.id"), nullable=True)
+    
     
     # Relationships
     doctor = relationship("Doctor")
