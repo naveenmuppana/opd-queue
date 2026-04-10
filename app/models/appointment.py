0724f024b7806da0fe0d0e95c51c8e1ea773a568
naveenmuppana <naveenmuppana123@gmail.com>
2026-04-10 23:03:35 +0530

    changes

--- app/models/appointment.py
+++ app/models/appointment.py
@@ -44,4 +44,5 @@ class WaitingList(Base):
     doctor      = relationship("Doctor")
     patient     = relationship("Patient")
     appointment = relationship("Appointment", back_populates="waiting_list_entry")
+    bb
     
